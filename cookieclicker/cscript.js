6ecd4598da963bcd15edf8ac2088af1e241ac75b
snakeMax <superknutsen@gmail.com>
2021-10-04 17:37:03 +0200

    made basic functionality
    for cookieclicker

--- cookieclicker/cscript.js
+++ cookieclicker/cscript.js
@@ -1,3 +1,8 @@
-function click() {
-    
+var cookiecount = 0;
+var multiplier = 1;
+var clickvalue = 1;
+
+function add() {
+    cookiecount = cookiecount + (clickvalue * multiplier);
+    document.getElementById("cookiecounter").innerHTML = "Cookie count: " + cookiecount;
 }
